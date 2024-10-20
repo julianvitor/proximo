800f50d0689eefe5b1d7150bf1af71257f28d5ca
WesleyPizetta <pizetta45@hotmail.com>
2024-10-20 17:04:18 -0300

    fix(Syntax): removendo espaço em branco desnecessário

--- Ali/app/src/main/java/com/proximo/ali/AdminActivity.kt
+++ Ali/app/src/main/java/com/proximo/ali/AdminActivity.kt
@@ -19,7 +19,7 @@ class AdminActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView( R.layout.activity_admin)
+        setContentView(R.layout.activity_admin)
 
         databaseHelper = DatabaseHelper(this)
 
